899b8c076a669a63204c8f0c24bb6a1fc8d1cc0c
Kacper Grabowski <w69973@student.wsiz.edu.pl>
2024-10-22 12:56:26 +0200

    Finished 2nd Lab - corrected#4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,6 +15,17 @@ internal class Program
         student.DodajOcene(4);
         student.DodajOcene(2);
         Console.WriteLine($"Średnia: {student.SredniaOcen}");
+        Licz v1 = new Licz(5);
+        v1.Dodaj(420);
+        Console.WriteLine($"Stan V1: {v1.Sprawdz()}");
+        List<int> list = new List<int>();
+        for(int i = 0; i<20; i++)
+        {
+            list.Add(i);
+        }
+        Sumator sum = new Sumator(list);
+        Console.WriteLine($"Suma: {sum.Suma()}");
+        Console.WriteLine($"Suma liczb podzielnych przez 2: {sum.SumaPodziel2()}");
 
     }
 }
