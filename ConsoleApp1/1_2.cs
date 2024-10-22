7618862acf0df0613f44d6eb27223397bbba872f
Kacper Grabowski <w69973@student.wsiz.edu.pl>
2024-10-22 12:54:00 +0200

    Finished 2nd Lab - corrected#3

--- ConsoleApp1/1_2.cs
+++ ConsoleApp1/1_2.cs
@@ -8,12 +8,15 @@ namespace ConsoleApp1
 {
     internal class Osoba
     {
-        string imie
-        { get => imie; set { if(value.Length >= 2) imie = value; } }
-        string nazwisko
-        { get => nazwisko; set { if (value.Length >= 2) nazwisko = value; } }
-        int wiek
-        { get => wiek; set { if(value > 0)  wiek = value; } }
+        string _imie;
+        public string imie
+        { get => _imie; set { if(value.Length >= 2) _imie = value; } }
+        string _nazwisko;
+        public string nazwisko
+        { get => _nazwisko; set { if (value.Length >= 2) _nazwisko = value; } }
+        int _wiek;
+        public int wiek
+        { get => _wiek; set { if(value > 0)  _wiek = value; } }
         public Osoba(string v1, string v2, int v3)
         {
             imie = v1;
